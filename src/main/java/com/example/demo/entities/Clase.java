0f8c3a92ee2a8639f71c9e2117fe9fdff6635d7b
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2023-12-27 12:26:40 -0500

    1

---
+++ src/main/java/com/example/demo/entities/Clase.java
@@ -0,0 +1,38 @@
+package com.example.demo.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Clase implements Serializable {
+    @Id
+    @SequenceGenerator(name = "clase_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "clase_id_seq")
+    private Integer id;
+    private String descripcion;
+    private String nuuid;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "clase", cascade = CascadeType.ALL)
+    private List<Jugador> jugadores = new ArrayList<>();
+}
